ecad3954442688a91acc9e345c7edd91de0060ff
Omar Abdallah <oamas857@gmail.com>
2026-04-24 01:12:36 +0300

    implemented adding and deleting nodes and edges

--- front/src/app/graph/graph.ts
+++ front/src/app/graph/graph.ts
@@ -1,37 +1,63 @@
 import { NonNullAssert } from '@angular/compiler';
 import { AfterViewInit, Component, computed, ElementRef, EventEmitter, OnDestroy, Output, Signal, signal, ViewChild } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
 
 import cytoscape from 'cytoscape';
-import { elementAt, every } from 'rxjs';
+import { elementAt, every, single } from 'rxjs';
 
 @Component({
 	selector: 'app-graph',
-	imports: [],
+	imports: [FormField],
 	standalone: true,
 	templateUrl: './graph.html',
 	styleUrl: './graph.css',
 })
 export class Graph implements AfterViewInit, OnDestroy {
+
 	private cy= signal<cytoscape.Core | undefined>(undefined)
 
 	@Output() calculate = new EventEmitter()
 
 	ngAfterViewInit(): void {
-		
+		this.initializeCytoscape()
+		this.initializeEvents()
+	}
+	initializeEvents = ()=> {
+		document.addEventListener('keydown', (event) => {
+		if (event.key == 'Shift' && event.shiftKey) {
+			this.shift.set(true)
+			console.log('shift set');
+		}
+		});
+		document.addEventListener('keyup', (event) => {
+		if (event.key == 'Shift') {
+			this.shift.set(false)
+			console.log('shift unset');
+		}
+		});
+	}
+	nodes = computed(() => this.cy()?.nodes().map((n: any) => n._private.data))
+	edges = computed(() => this.cy()?.edges().map((n: any) => n._private.data))
+	ngOnDestroy(): void {
+		if (this.cy) {
+			this.cy()?.destroy();
+		}
+	}
+	shift = signal(false);
+	
+	initializeCytoscape = () => {
 		this.cy.set( cytoscape({
 			container: document.getElementById('cy'),
 			elements: [
 				// Define nodes
-				{ data: { id: 'A', label: 'Input' } },
-				{ data: { id: 'B', label: 'Task 1' } },
-				{ data: { id: 'C', label: 'Task 2' } },
-				{ data: { id: 'D', label: 'Output' } },
+				{ data: { id: 'input', label: 'input' } },
+				{ data: { id: 'output', label: 'output' } },
 				// Define edges (directed)
-				{ data: { id: 'AB', source: 'A', target: 'B' } },
-				{ data: { id: 'AC', source: 'A', target: 'C' } },
-				{ data: { id: 'BD', source: 'B', target: 'D' } },
-				{ data: { id: 'BB', source: 'B', target: 'B' } },
-				{ data: { id: 'CD', source: 'C', target: 'D', value: 2 } }
+/* 				{ data: { id: 'AB', source: 'A', target: 'B', weight: 2 } },
+				{ data: { id: 'AC', source: 'A', target: 'C', weight: 2 } },
+				{ data: { id: 'BD', source: 'B', target: 'D', weight: 2 } },
+				{ data: { id: 'BB', source: 'B', target: 'B', weight: 2 } },
+				{ data: { id: 'CD', source: 'C', target: 'D', weight: 2 } } */
 			],
 			style: [
 				{
@@ -59,7 +85,8 @@ export class Graph implements AfterViewInit, OnDestroy {
 						'curve-style': 'bezier',
 						'loop-direction': '0',
 						'loop-sweep': '-250',
-						
+						'label': 'data(weight)',
+						'text-margin-y': -10
 					}
 				}
 			],
@@ -72,45 +99,106 @@ export class Graph implements AfterViewInit, OnDestroy {
 			}
 		}));
 
-		this.cy()?.on('click', 'node', (event: any) => {
-			const node = event.target;
-			console.log('clicked on a node');
-			console.log('ev: ');
-			console.log(event);
-		})
+
 		this.cy()?.on('tap', (event: any) => {
 			const target = event.target
 			if (target == this.cy()) {
 				console.log('clicked on the background');
+				if(this.selected())
+					this.selected().style("background-color", '#ffffff')
+				this.selected.set(null)
+
 			} else {
 				console.log('clicked on something else');
-				console.log(target);
+				
+				if(target.isNode()) {
+					if(!this.shift() || !this.selected() || this.selected().isEdge()) {
+						
+						if(this.selected()) {
+							if(this.selected().isNode())
+								this.selected().style("background-color", '#ffffff')
+							else
+								this.selected().style("line-color", 'black')
+						}
+						target.style("background-color", '#90EE90')
+						this.selected.set( target);
+					}else {
+						//{ data: { id: 'CD', source: 'C', target: 'D', weight: 2 } }
+						this.cy()?.add({ data: { id: generateUUID(), source: this.selected().id(), target: target.id(), weight: this.inputWeight() } })
+					}
+				}else {
+					// edges
+					// 'line-color': 'black'						
+					if(this.selected()) {
+						if(this.selected().isNode())
+							this.selected().style("background-color", '#ffffff')
+						else
+							this.selected().style("line-color", 'black')
+					}
+					target.style("line-color", '#90EE90')
+					this.selected.set( target);
+				}
 			}
 		})
 		this.cy()?.on('dbltap', (event: any) => {
 			const target = event.target
 			if (target == this.cy()) {
 				console.log('clicked on the background twice');
-				console.log(event.position);
 				
-				this.cy()?.add({ data: { id: 'Z', label: 'Input'}, position: event.position})
+				this.cy()?.add({ data: { id: generateUUID(), label: this.getNextLabel()}, position: event.position})
 			} else {
 				console.log('clicked on something else twice');
-				console.log(target);
+				console.log(target.label());
 			}
 		})
 		// don't forget one()
-		console.log(this.cy()?.nodes().map((n: any) => n._private.data));
-		console.log(this.cy()?.edges().map((n: any) => n._private.data));
-		
+		// console.log(this.cy()?.nodes().map((n: any) => n._private.data));
+		// console.log(this.cy()?.edges().map((n: any) => n._private.data));
 		
 	}
-	nodes = computed(() => this.cy()?.nodes().map((n: any) => n._private.data))
-	edges = computed(() => this.cy()?.edges().map((n: any) => n._private.data))
-	ngOnDestroy(): void {
-		if (this.cy) {
-			this.cy()?.destroy();
+	selected = signal<any>(null);
+	inputWeight = signal(0)
+	inputChangeHandler = ($event: any)=> {
+		this.inputWeight.set($event.target.value)
+		// console.log($event.target.value);
+	}
+	labels: Record<number, boolean> = {}
+	getNextLabel = (): number=> {
+		let i = 1
+		for(;i<1e10; i++) {
+			if(!this.labels[i])
+				break
 		}
+		this.labels[i] = true
+		return i
 	}
+
+	handleDelete = () => {
+		if(!this.selected())
+			return
+		const label = this.selected()._private.data.label
+		if(label == 'input' || label == 'output') {
+			alert(`You can't delete the ${label} node!!!`)
+			return
+		}
+		this.cy()?.remove(this.selected())
+		console.log(this.selected());
+		
+		this.labels[label] = false
+		this.selected.set(null)
+		/* console.log(this.cy()?.nodes().map((n: any) => n._private.data));
+		console.log(this.cy()?.edges().map((n: any) => n._private.data)); */
+	}
+	getNodes = () => this.cy()?.nodes().map((n: any) => n._private.data)
+	getEdges = () => this.cy()?.edges().map((n: any) => n._private.data)
+}
+
+function generateUUID() {
+  const bytes = crypto.getRandomValues(new Uint8Array(16));
+  // Set version to 4 and variant to 10 (RFC 4122)
+  bytes[6] = (bytes[6] & 0x0f) | 0x40;
+  bytes[8] = (bytes[8] & 0x3f) | 0x80;
+  const hex = Array.from(bytes).map(b => b.toString(16).padStart(2, '0')).join('');
+  return `${hex.slice(0,8)}-${hex.slice(8,12)}-${hex.slice(12,16)}-${hex.slice(16,20)}-${hex.slice(20)}`;
 }
 
